fd8e4a8d0dea33d37e9f89e77497f1be6580aa30
Michael Hayden <mhayden@Michaels-iMac.hsd1.co.comcast.net>
2020-12-25 12:13:57 -0700

    init typscript app

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
