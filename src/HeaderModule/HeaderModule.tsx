b2a16bf41b0aea74d957ebb033831ab1e3e8fc21
Michael Hayden <mhayden@michaelhayden.net>
2021-01-18 20:30:44 -0700

    remove coverage folder

--- src/HeaderModule/HeaderModule.tsx
+++ src/HeaderModule/HeaderModule.tsx
@@ -2,9 +2,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import SplashScreenModule from "../SplashScreenModule/SplashScreenModule";
 
 export default function HeaderModule() {
+    let name = 'Mike';
+    if(name === 'Mike') {
+        name = 'Rb';
+    }
+
   return (
     <View style={styles.container}>
-      {/*<Text>MOBILE :: HEADER</Text>*/}
+      <Text>{name} MOBILE :: HEADER</Text>
       <SplashScreenModule />
     </View>
   );
